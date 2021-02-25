637387329ffd652e69d81d8f701ad3158040275f
Bruno Alves <dossantosbrunoalves@gmail.com>
2021-02-24 23:11:35 -0300

    teste prd

--- 02-dashboard/auth/src/App.js
+++ 02-dashboard/auth/src/App.js
@@ -11,31 +11,19 @@ const generateClassName = createGenerateClassName({
 
 export default ({ onSignIn, history }) => {
   return (
-    // <div>
-    //   <StylesProvider generateClassName={generateClassName}>
-    //     <Router history={history}>
-    //       <Switch>
-    //         <Route path="/auth/signin">
-    //           <Signin onSignIn={onSignIn} />
-    //         </Route>
-    //         <Route path="/auth/signup">
-    //           <Signup onSignIn={onSignIn} />
-    //         </Route>
-    //       </Switch>
-    //     </Router>
-    //   </StylesProvider>
-    // </div>
     <div>
-      <Router history={history}>
-        <Switch>
-          <Route path="/auth/signin">
-            <div>SIGN IN</div>
-          </Route>
-          <Route path="/auth/signup">
-            <div>SIGN UP</div>
-          </Route>
-        </Switch>
-      </Router>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
     </div>
   )
 }
